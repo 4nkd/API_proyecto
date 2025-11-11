63fa45c879320a7c476e7416787cf4b27a718e74
QUIROZ <kq152004@gmail.com>
2025-11-11 10:03:46 -0500

    implementacion del estadistica del uso de apps

--- src/controllers/usoController.js
+++ src/controllers/usoController.js
@@ -34,6 +34,79 @@ export const getUsosByUser = async (req, res) => {
     };
 }
 
+export const postUsoDiarioApp = async (req, res) => {
+    try {
+        const {fecha, total_minutos} = req.body;
+
+        if (!fecha || !total_minutos){
+            return res.status(400).json({ message: "Faltan campos obligatorios" });
+        }
+
+        await conmysql.query(
+            'INSERT INTO Uso_App_Diario (id_uso, fecha, total_minutos) VALUES (?, ?, ?)',
+            [req.params.id, fecha, total_minutos]
+        );
+        res.status(201).json({ message: "uso de diario de aplicacion registrado correctamente"});
+    }catch (error){
+        return res.status(500).json({ message: 'Error in the server: ' + error.message });
+    };
+}
+
+
+export const getUsoDiarioApp = async (req, res) => {
+    try {
+        const [result] = await conmysql.query(
+            'SELECT * from uso_app_diario WHERE id_uso = ?;',
+            [req.params.id]);
+        if(result.length <= 0) return res.json({
+            cant: 0,
+            message: 'No hay uso registrados de la app'
+        });
+        res.json({
+            cant: result.length,
+            data: result
+        })
+    }catch (error){
+        return res.status(500).json({ message: 'Error in the server: ' + error.message });
+    };
+}
+
+export const getPromedioUsoApp = async (req, res) => {
+    try {
+        const {dias} = req.body;
+
+        const [result_prom] = await conmysql.query(
+            'SELECT estadisticaUsoApp(?, ?) AS promedio_uso;',
+            [req.params.id, dias]);
+
+        const [result_total] = await conmysql.query(
+            'SELECT COALESCE(SUM(total_minutos), 0) FROM Uso_App_Diario WHERE id_uso = ? AND fecha BETWEEN DATE_SUB(CURDATE(), INTERVAL ? DAY) AND CURDATE();',
+            [req.params.id, dias]);
+
+        if(result_prom.length <= 0 ) return res.json({
+            cant: 0,
+            message: 'Error al conseguir promedio'
+        });
+        if(result_total.length <= 0 ) return res.json({
+            cant: 0,
+            message: 'Error al conseguir total'
+        });
+
+        res.json({
+            cant: {
+                cant_prom: result_prom.length,
+                cant_total: result_total.length
+            },
+            data: {
+                data_prom: result_prom,
+                data_total: result_total
+            }
+        })
+    }catch (error){
+        return res.status(500).json({ message: 'Error in the server: ' + error.message });
+    };
+}
+
 ///generarEstadisticasUso() esto tambien se podria hacer una funcion de mysql
 
 
--- src/routes/uso.routes.js
+++ src/routes/uso.routes.js
@@ -1,16 +1,22 @@
 import {Router} from 'express';
-import {getUsosByUser, postUsoApp} from '../controllers/usoController.js'
+import {getUsosByUser, postUsoApp, postUsoDiarioApp, getUsoDiarioApp, getPromedioUsoApp} from '../controllers/usoController.js'
 import {verifyToken} from '../middleware/auth.middleware.js'
 
 const router=Router();
 
-router.get('/usoAplicaion/:id', verifyToken, getUsosByUser);//get users by id (obviusly)
-router.post('/usoAplicaion/:id', verifyToken, postUsoApp); //el post pues
+router.get('/usoAplicacion/:id', verifyToken, getUsosByUser);//get users by id (obviusly)
+router.post('/usoAplicacion/:id', verifyToken, postUsoApp); //el post pues
 
+//pude crear otro routes y controller, pero me dio hueva
+router.post('/usoAplicacion/diario/:id', verifyToken, postUsoDiarioApp) // registra el uso del día
+router.get('/usoAplicacion/diario/:id', verifyToken, getUsoDiarioApp) //el uso diario de aquella app
 
+router.get('/usoAplicacion/diario/promedio/:id', verifyToken, getPromedioUsoApp)//promedio en fecha determinada
 
 //estadistica de uso??????? ---> modificar tabla, debe de haber un progreso del uso de la aplicacion 
 
+
 //verificarLimiteUso() → si excede, llama a crearAlerta() <--- no me termino de idear como seria
+// creo que esto seria en el cliente, osea en la app movil
 
 export default router;
