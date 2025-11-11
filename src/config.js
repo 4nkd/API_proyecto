07136c68eb0c82f2f8a1347dbd6f592fefc6852b
QUIROZ <kq152004@gmail.com>
2025-11-10 19:57:00 -0500

    Cambios para el render...

--- src/config.js
+++ src/config.js
@@ -1,16 +1,16 @@
-import {config} from 'dotenv'
-config()
+import { config } from 'dotenv';
+config();
 
-export const BD_HOST=process.env.BD_HOST || 'mysql-project-kq152004-cbe1.k.aivencloud.com'
-export const BD_DATABASE=process.env.BD_DATABASE || 'proyecto'
-export const BD_USER=process.env.BD_USER ||'avnadmin'
-export const BD_PASSWORD=process.env.BD_PASSWORD || 'AVNS_DGirkqOuaZiFJjfK2bu'
-export const BD_PORT=process.env.BD_PORT || 10210;
+// Variables de conexión a la base de datos
+export const BD_HOST = process.env.BD_HOST;
+export const BD_DATABASE = process.env.BD_DATABASE;
+export const BD_USER = process.env.BD_USER;
+export const BD_PASSWORD = process.env.BD_PASSWORD;
+export const BD_PORT = process.env.BD_PORT;
 
+// Puerto de la API
+export const PORT = process.env.PORT || 10000;
 
-export const PORT=process.env.PORT || 10000;
-
-
-//JWT
-export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || 'Qe^St!Q92Z99!mJKS4000zt^vPn6C';
-export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '30d'
+// JWT (autenticación)
+export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '30d';
